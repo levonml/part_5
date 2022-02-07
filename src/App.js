78a5f24f99358b26d0c99a7a6994a2bb4be53cd5
Levon Stepanyan <levonmll@gmail.com>
2022-02-07 22:12:21 +0200

    add create new blog functionaliti ex 5.3

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ const App = () => {
 	const [user, setUser] = useState(null);
 	const [userName, setUsername] = useState("");
 	const [password, setPassword] = useState("");
+	const [title, setTitle] = useState("");
+	const [author, setAuthor] = useState("");
+	const [url, setUrl] = useState("");
 
 	useEffect(() => {
 		(async () => {
 			try {
-				console.log("inside the f");
 				const reseivedBlogs = await blogService.getAll();
 				setBlogs(reseivedBlogs);
 			} catch (err) {
@@ -26,7 +28,7 @@ const App = () => {
 			const user = await loginService.login({ userName, password });
 			setUsername("");
 			setPassword("");
-			user && setUser(user);
+			setUser(user);
 			user && window.localStorage.setItem("loggedUser", JSON.stringify(user));
 		} catch (err) {
 			console.log("login error", err);
@@ -37,17 +39,32 @@ const App = () => {
 		window.localStorage.clear();
 		console.log("in local storsge", window.localStorage.getItem("loggedUser"));
 	};
+	const handleCreate = async (event) => {
+		event.preventDefault();
+		const newBlog = {
+			title,
+			author,
+			url,
+		};
+		try {
+			const receivedBlog = await blogService.create(newBlog);
+			//setBlogs(blogs.concat(reseivedBlogs));
+			console.log("reseived New Blog is ", receivedBlog);
+		} catch (err) {
+			console.log("error from getall", err);
+		}
+	};
 	useEffect(() => {
 		let loggedUser = window.localStorage.getItem("loggedUser");
 		if (loggedUser) {
 			loggedUser = JSON.parse(loggedUser);
 			setUser(loggedUser);
-			console.log("logged ser isss", loggedUser);
-			//blogService.setToken(user.token);
+
+			blogService.setToken(loggedUser.token);
 		}
 	}, []);
+	console.log("useeeeeeeeeeeeer iiis", user);
 	const blogsList = () => {
-		//getBlogs();
 		return (
 			<div>
 				<div>
@@ -64,6 +81,38 @@ const App = () => {
 			</div>
 		);
 	};
+	const createNewBlog = () => (
+		<form onSubmit={handleCreate}>
+			<h2>create new blog</h2>
+			<div>
+				title :
+				<input
+					value={title}
+					name="Title"
+					onChange={({ target }) => setTitle(target.value)}
+				/>
+			</div>
+			<div>
+				author:
+				<input
+					value={author}
+					name="Author"
+					onChange={({ target }) => setAuthor(target.value)}
+				/>
+			</div>
+			<div>
+				url:
+				<input
+					value={url}
+					name="Url"
+					onChange={({ target }) => setUrl(target.value)}
+				/>
+			</div>
+			<button type="submit" onClick={handleCreate}>
+				create
+			</button>
+		</form>
+	);
 
 	const loginForm = () => (
 		<form onSubmit={handleLogin}>
@@ -92,7 +141,12 @@ const App = () => {
 	if (user === null) {
 		return <div>{loginForm()}</div>;
 	}
-	return <div>{blogsList()}</div>;
+	return (
+		<>
+			<div>{createNewBlog()}</div>
+			<div>{blogsList()}</div>
+		</>
+	);
 };
 
 export default App;
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -9,6 +9,18 @@ const getAll = async () => {
 		console.log("error from getall", err);
 	}
 };
-
-const blogService = { getAll };
+let config = null;
+const setToken = (token) => {
+	console.log("token will beeee", token);
+	config = { headers: { Authorization: `Bearer ${token}` } };
+};
+const create = async (newBlog) => {
+	try {
+		const respons = await axios.post(baseUrl, newBlog, config);
+		return respons.data;
+	} catch (err) {
+		console.log("create error", err);
+	}
+};
+const blogService = { getAll, setToken, create };
 export default blogService;
